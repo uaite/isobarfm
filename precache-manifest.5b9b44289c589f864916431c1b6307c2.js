fccce30bc3061cef43d6e9dbb498afdb1ed44b3a
i848465 <e.carvalho@sap.com>
2020-01-19 21:38:09 -0300

    Updates

---
+++ precache-manifest.5b9b44289c589f864916431c1b6307c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ebaf3878e4f903d296ab83c6ee1ac6",
+    "url": "/isobarfm/index.html"
+  },
+  {
+    "revision": "e8b9b41359f6754db620",
+    "url": "/isobarfm/static/js/2.70de18fa.chunk.js"
+  },
+  {
+    "revision": "b9e71b55ca1940274e8f",
+    "url": "/isobarfm/static/js/main.18b9f15b.chunk.js"
+  },
+  {
+    "revision": "b272d0fcc27c49427f8b",
+    "url": "/isobarfm/static/js/runtime-main.091823b2.js"
+  },
+  {
+    "revision": "40d312e2d29116f6a1bbbf29e64bb33d",
+    "url": "/isobarfm/static/media/logo.40d312e2.png"
+  },
+  {
+    "revision": "3ffe858484a92895cff7b789c92ee0fb",
+    "url": "/isobarfm/static/media/no_results.3ffe8584.png"
+  },
+  {
+    "revision": "3e6da4021fae5f02865ab78232af46cc",
+    "url": "/isobarfm/static/media/search.3e6da402.png"
+  }
+]);
