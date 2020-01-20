9f8c554d06a473f0180736cdb2eb22b3aa914c56
i848465 <e.carvalho@sap.com>
2020-01-19 21:47:58 -0300

    feat: Show filter status in BandList

--- src/components/BandList/index.js
+++ src/components/BandList/index.js
@@ -3,19 +3,23 @@ import React, { Fragment, useEffect } from 'react';
 import Band from '../Band';
 import Loader from '../Loader';
 import Error from '../Error';
-import { BandListContainer } from './style';
+import { BandListWrapper, BandListContainer } from './style';
 
 import { useSelector } from 'react-redux';
 import useActions from '../../hooks/useActions';
 
 import * as bandsSelectors from '../../selectors/bands';
 import * as bandsActions from '../../actions/bands';
+import { SORTING_MODES } from '../../reducers/bands';
 
 const BandList = () => {
   const filter = useSelector(bandsSelectors.getFilter);
   const filteredBands = useSelector(bandsSelectors.getFilteredBands);
   const error = useSelector(bandsSelectors.getError);
   const isLoading = useSelector(bandsSelectors.isBandListLoading);
+
+  const sort = useSelector(bandsSelectors.getSort);
+  const ascending = useSelector(bandsSelectors.getAscending);
 
   const fetchBands = useActions(bandsActions.fetchBands);
 
@@ -27,14 +31,35 @@ const BandList = () => {
     return filteredBands.map(value => <Band key={value.id} {...value} />);
   };
 
+  const getSortText = () => {
+    let text;
+    switch (sort) {
+      case SORTING_MODES.ALPHABETICAL:
+        text = 'sorted alphabetically';
+        break;
+      case SORTING_MODES.POPULARITY:
+        text = 'sorted by popularity';
+        break;
+      default:
+        return '';
+    }
+
+    return `${text} in ${ascending ? 'ascending' : 'descending'} order`;
+  };
+
   const renderList = () => {
     if (filter && filteredBands.length === 0) {
       return <Error message={'No results to show.'}></Error>;
     }
     return (
-      <BandListContainer>
-        {error ? <Error message={error.message} /> : listBands()}
-      </BandListContainer>
+      <BandListWrapper>
+        <p>
+          Showing {filteredBands.length} results {getSortText()}
+        </p>
+        <BandListContainer>
+          {error ? <Error message={error.message} /> : listBands()}
+        </BandListContainer>
+      </BandListWrapper>
     );
   };
 
--- src/components/BandList/style.js
+++ src/components/BandList/style.js
@@ -1,9 +1,21 @@
 import styled from 'styled-components';
 import { CardListContainer } from '../Card';
 
-export const BandListContainer = styled(CardListContainer)``;
+export const BandListWrapper = styled.div`
+  padding: 3vh 5vw;
+  p {
+    font-size: 1.2em;
+    color: ${props => props.theme.black};
+    text-align: right;
 
-// ::after {
-//   content: '';
-//   flex: auto;
-// }
+    @media (min-width: 1024px) {
+      font-size: 1em;
+    }
+  }
+`;
+BandListWrapper.displayName = 'BandListWrapper';
+
+export const BandListContainer = styled(CardListContainer)`
+  padding: 0;
+`;
+BandListContainer.displayName = 'BandListContainer';
